e51357e6a8d1f6a8d2f2fdc659681a488109ce8c
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-17 13:39:06 +0530

    created folder and file str for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
 "use client"
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWorks";
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-    </div>
+    <section className="min-h-screen bg-background">
+      <Header />
+      <Hero />
+      <HowItWorks />
+      <WhatToAsk />
+      <PricingSection />
+      <CTA />
+      <Footer />
+    </section>
   );
 }
