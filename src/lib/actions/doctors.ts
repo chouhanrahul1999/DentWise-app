474425df7d0110fc4b812c994bf0b19607baa3cf
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-20 01:35:09 +0530

    created the server action to update doctor

--- src/lib/actions/doctors.ts
+++ src/lib/actions/doctors.ts
@@ -4,6 +4,7 @@ import { Gender } from "@prisma/client";
 import { prisma } from "../prisma";
 import { generateAvatar } from "../utils";
 import { revalidatePath } from "next/cache";
+import { th } from "date-fns/locale";
 
 const getDoctors = async () => {
   try {
@@ -56,5 +57,50 @@ export const createDoctor = async (input: CreateDoctorInput) => {
     }
     console.error("Error creating doctor:", e);
     throw new Error("Failed to create doctor");
+  }
+};
+
+interface updateDoctorInput extends Partial<CreateDoctorInput> {
+  id: string;
+}
+
+export const updateDoctor = async (input: updateDoctorInput) => {
+  try {
+    if (!input.name || !input.email)
+      throw new Error("Name and email are required");
+
+    const currentDoctor = await prisma.doctor.findUnique({
+      where: { id: input.id },
+      select: { email: true },
+    });
+
+    if (!currentDoctor) throw new Error("Doctor not found");
+
+    if(input.email !== currentDoctor.email) {
+        const existingDoctor = await prisma.doctor.findUnique({
+            where: { email: input.email },
+          });
+
+        if (existingDoctor) {
+            throw new Error("Doctor with this email already exists");
+        }
+    }
+
+    const doctor = await prisma.doctor.update({
+      where: { id: input.id },
+      data: {
+        name: input.name,
+        email: input.email,
+        phone: input.phone,
+        speciality: input.speciality,
+        gender: input.gender,
+        isActive: input.isActive,
+      },
+    });
+
+    return doctor;
+  } catch (e) {
+    console.error("Error updating doctor:", e);
+    throw new Error("Failed to update doctor");
   }
 };
