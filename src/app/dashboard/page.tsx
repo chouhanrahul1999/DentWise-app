03e4075ed4b8853b68d7cd50a221ce8f5f280f2f
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-23 14:43:22 +0530

    created welcome section for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,16 @@
+import WelcomeSection from '@/components/dashboard/WelcomeSection'
 import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='min-h-screen bg-background'>
+    <>
       <Navbar />
       Dashboard
-    </div>
+      <div className='max-w-7xl mx-auto px-6 py-8 pt-24'>
+        <WelcomeSection />
+      </div>
+    </>
   )
 }
 
