85de5fdf8053db3d75b6cf68ae112800c39baad7
RahulSingh <chouhanrahul1999@gmail.com>
2026-02-23 14:50:28 +0530

    created main action components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import MainActions from '@/components/dashboard/MainActions'
 import WelcomeSection from '@/components/dashboard/WelcomeSection'
 import Navbar from '@/components/Navbar'
 import React from 'react'
@@ -9,6 +10,7 @@ const Dashboard = () => {
       Dashboard
       <div className='max-w-7xl mx-auto px-6 py-8 pt-24'>
         <WelcomeSection />
+        <MainActions />
       </div>
     </>
   )
